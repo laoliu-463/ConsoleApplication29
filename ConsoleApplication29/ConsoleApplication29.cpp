8b546324884076e41c19d5a8961ee332e253ee48
laoliu-463 <caojianing463@gmail.com>
2025-03-20 20:21:25 +0800

    111

--- ConsoleApplication29/ConsoleApplication29.cpp
+++ ConsoleApplication29/ConsoleApplication29.cpp
@@ -1,14 +1,61 @@
 ﻿#include <iostream>
-
+#include<graphics.h>
+#include<conio.h> // 添加此行以包含 _getch() 的定义
 using namespace std;
+void drawTEXT() {
+    ExMessage msg = { 0 };
+    while (true) {
+        system("cls");
+        int rx = 30;
+        int ry = 380;
+        int rw = 200;
+        int rh = 60;
+        setfillcolor(BLUE);
+        fillrectangle(rx, ry, rw + rx, ry + rh);
 
-int main()
-{
-  
+
+        setbkmode(TRANSPARENT);
+        int height = (rw - textwidth(_T("hello,easyx"))) / 2;
+        int width = (rh - textheight(_T("hello,easyx"))) / 2;
+        settextcolor(RED);
+        outtextxy(rx + height, ry + width, _T("hello,easyx"));
+        
+       if (peekmessage(&msg, EX_MOUSE)) { // 修复此行，添加分号并更改为 if 语句
+
+
+            if (msg.message == WM_LBUTTONDOWN) {
+                printf("click");
+            }
+
+        }
     }
-//int save_to_file(const char* filename, Student* students, int count);
-//int load_from_file(const char* filename, Student** students, int* count);
- //void display_menu();
-//void handle_choice(Student** students, int* count, int choice);
+   
+    int rx=30;
+    int ry = 380;
+    int rw = 200;
+    int rh = 60;
+    setfillcolor(BLUE);
+    fillrectangle(rx,ry, rw+rx, ry+rh);
+    setbkmode(TRANSPARENT);
+    int height = (rw - textwidth(_T("hello,easyx")))/2;
+    int width =( rh- textheight(_T("hello,easyx")))/2;
+    settextcolor(RED);
+    outtextxy(rx+height,ry+width,_T("hello,easyx"));
+}
+int main(){
+    // 初始化图形窗口，大小为 640x480
+    initgraph(1000,600);
+    setbkcolor(WHITE);
 
+    drawTEXT();
 
+ 
+  
+
+    // 等待用户按键
+    _getch();
+
+    // 关闭图形窗口
+    closegraph();
+
+}
